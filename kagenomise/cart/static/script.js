81ef8abf96eeae660ae4b3f4a5206fb5d89a1df1
Izhar Firdaus <izhar@inigo-tech.com>
2013-05-18 19:37:07 +0800

    added js resources

---
+++ kagenomise/cart/static/script.js
@@ -0,0 +1,27 @@
+/* script.js */
+
+$(document).ready(function () {
+    simpleCart.currency({
+        code: 'MYR',
+        name: 'Malaysian Ringgit',
+        symbol: 'MYR'
+    });
+    simpleCart({
+        checkout: {
+            type: 'SendForm',
+            url: $('#kagenomise-cartmetadata').attr('checkout-url')
+        },
+        cartStyle: 'table',
+        currency: 'MYR',
+            cartColumns: [
+                { attr: "name" , label: "Name" } ,
+                { attr: "size" , label: "Size" } ,
+                { attr: "price" , label: "Price", view: 'currency' } ,
+                { view: "decrement" , label: false , text: "-" } ,
+                { attr: "quantity" , label: "Qty" } ,
+                { view: "increment" , label: false , text: "+" } ,
+                { attr: "total" , label: "SubTotal", view: 'currency' } ,
+                { view: "remove" , text: "Remove" , label: false }
+        ]
+    })
+});
