91a3f5879bed41ed4b906f9f537a999c4b62d18d
Izhar Firdaus <izhar@inigo-tech.com>
2013-05-19 16:57:53 +0800

    added upgrade profile to 1001

--- kagenomise/cart/upgrades/handlers.py
+++ kagenomise/cart/upgrades/handlers.py
@@ -2,3 +2,12 @@ from collective.grok import gs
 from Products.CMFCore.utils import getToolByName
 
 # -*- extra stuff goes here -*- 
+
+
+@gs.upgradestep(title=u'Upgrade kagenomise.cart to 1001',
+                description=u'Upgrade kagenomise.cart to 1001',
+                source='1', destination='1001',
+                sortkey=1, profile='kagenomise.cart:default')
+def to1001(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-kagenomise.cart.upgrades:to1001')
